0441893e63cad2271880fd47f0ca8de34be892a7
saraacarsoy <bayrakkutluhan@gmail.com>
2024-01-11 13:48:24 +0100

    Custom Game Fetch Hook

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -3,6 +3,6 @@ import axios from "axios";
 export default axios.create({
   baseURL: "https://api.rawg.io/api",
   params: {
-    key: "7013d39567b445d6a034d54ec9a31d96",
+    key: "22dd76f0b9054ce5ba37afe02e52b755",
   },
 });
