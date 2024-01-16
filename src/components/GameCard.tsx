d3f1a72710e4676902a3bd70ded762ad22d046b0
saraacarsoy <bayrakkutluhan@gmail.com>
2024-01-16 15:10:53 +0100

    Game Card Clean-up

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -22,8 +22,7 @@ const GameCard = ({ game }: Props) => {
     <Card>
       <Image src={getCroppedImage(game.background_image)} />
       <CardBody>
-        <Heading fontSize="2xl">{game.name}</Heading>
-        <HStack justifyContent="space-between">
+        <HStack justifyContent="space-between" marginBottom={1}>
           <PlatformIconList
             platforms={game.parent_platforms.map(
               (platform) => platform.platform
@@ -32,6 +31,7 @@ const GameCard = ({ game }: Props) => {
           />
           <CriticScore score={game.metacritic} />
         </HStack>
+        <Heading fontSize="2xl">{game.name}</Heading>
       </CardBody>
     </Card>
   );
