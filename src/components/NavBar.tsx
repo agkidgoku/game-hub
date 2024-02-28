b9342669ea3de7955e28802832dd66f380138efc
saraacarsoy <bayrakkutluhan@gmail.com>
2024-02-28 22:56:38 +0100

    Added more functionality to home button

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -3,11 +3,21 @@ import logo from "../assets/logo.webp";
 import ColorModeSwitch from "./ColorModeSwitch";
 import SearchInput from "./SearchInput";
 import { Link } from "react-router-dom";
+import useGameQuery from "../hooks/store";
 
 const NavBar = () => {
+  const setSelectedGenre = useGameQuery((s) => s.setGenre);
+  const setSelectedPlatformId = useGameQuery((s) => s.setPlatform);
+
   return (
     <HStack padding="10px">
-      <Link to={"/"}>
+      <Link
+        to={"/"}
+        onClick={() => {
+          setSelectedGenre();
+          setSelectedPlatformId();
+        }}
+      >
         <Image src={logo} boxSize="60px" objectFit={"cover"} />
       </Link>
       <SearchInput />
